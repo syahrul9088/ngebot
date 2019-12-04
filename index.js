b203017ab52cc2c225beb41a0d5046dad14d70d4
syahrul9088 <39154644+syahrul9088@users.noreply.github.com>
2019-12-04 10:13:27 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,84 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const delay = require('delay');
+const readlineSync = require('readline-sync');
+const randomstring = require("randomstring");
+const { URLSearchParams } = require('url');
+
+const getCookie = (reff) => new Promise((resolve, reject) => {
+    fetch(`https://techobible.com/register?ref=${reff}`, {
+        method: 'GET',
+    })
+    .then(async res => {
+        const $ = cheerio.load(await res.text());
+        const result = {
+            cookie: res.headers.raw()['set-cookie']
+        }
+
+        resolve(result)
+    })
+    .catch(err => reject(err))
+});
+
+const functionRegist = (realCookie, kelamin) => new Promise((resolve, reject) => {
+    const params = new URLSearchParams;
+    params.append('username', randomstring.generate(7));
+    params.append('email', `${randomstring.generate(7)}@gmail.com`);
+    params.append('password', 'Asdklmqwe987');
+    params.append('confirm_password', 'Asdklmqwe987');
+    params.append('gender', kelamin);
+    params.append('accept_terms', 'on');
+    
+    fetch('https://techobible.com/requests.php?f=register', { 
+        method: 'POST', 
+        body: params,
+        headers: {
+            'Host': 'techobible.com',
+            'Accept': '*/*',
+            'Origin': 'https://techobible.com',
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Referer': 'https://techobible.com/register?ref=Syahrul9088',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Cookie': realCookie
+
+        }
+    })
+    .then(res => res.json())
+    .then(result => {
+        //const $ = cheerio.load(result);
+        //const resText = $('h7').text();
+            resolve(result);
+    })
+    .catch(err => reject(err))
+});
+
+(async () => {
+    const reff = readlineSync.question('[?] Reff id: ')
+    const jumlah = readlineSync.question('[?] Jumlah reff: \n')
+    for (var i = 0; i < jumlah; i++) {
+    try {
+        var jenis = [   
+            "male",
+            "female",
+          ];
+        var kelamin = jenis[Math.floor(Math.random()*jenis.length)];
+        const cookie = await getCookie(reff)
+        const cook = cookie.cookie.join().split(',');
+        const session = cook[0].split(';')[0];
+        const ad = cook[3].split(';')[0]
+        const us = cook[5].split(';')[0]
+        const realCookie = `${session}; ${ad}; mode=day; access=1; src=1; _ga=GA1.2.79560195.1575427248; _gid=GA1.2.1128287706.1575427248; _gat_gtag_UA_148732261_1=1; ${us}`
+        const regist = await functionRegist(realCookie, kelamin)
+        if (regist.status == 200) {
+            console.log('[+] Register berhasil!')
+        } else {
+            console.log(regist)
+        }
+    } catch (e) {
+        console.log(e)
+  }
+}
+})()
