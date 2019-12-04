9e8c932fc23091820caa54d640667582b550f44b
syahrul9088 <39154644+syahrul9088@users.noreply.github.com>
2019-12-04 10:21:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const readlineSync = require('readline-sync');
 const randomstring = require("randomstring");
 const { URLSearchParams } = require('url');
 
+const functionName = () => new Promise((resolve, reject) => {
+
+    fetch('https://uinames.com/api/?region=indonesia', { 
+        method: 'GET'
+    })
+    .then(res => res.json())
+    .then(result => {
+        resolve(result);
+    })  
+    .catch(err => reject(err))
+});
+
 const getCookie = (reff) => new Promise((resolve, reject) => {
     fetch(`https://techobible.com/register?ref=${reff}`, {
         method: 'GET',
@@ -20,10 +32,10 @@ const getCookie = (reff) => new Promise((resolve, reject) => {
     .catch(err => reject(err))
 });
 
-const functionRegist = (realCookie, kelamin) => new Promise((resolve, reject) => {
+const functionRegist = (username, realCookie, kelamin) => new Promise((resolve, reject) => {
     const params = new URLSearchParams;
-    params.append('username', randomstring.generate(7));
-    params.append('email', `${randomstring.generate(7)}@gmail.com`);
+    params.append('username', username);
+    params.append('email', `${username}@gmail.com`);
     params.append('password', 'Asdklmqwe987');
     params.append('confirm_password', 'Asdklmqwe987');
     params.append('gender', kelamin);
@@ -57,9 +69,12 @@ const functionRegist = (realCookie, kelamin) => new Promise((resolve, reject) =>
 
 (async () => {
     const reff = readlineSync.question('[?] Reff id: ')
-    const jumlah = readlineSync.question('[?] Jumlah reff: \n')
+    const jumlah = readlineSync.question('[?] Jumlah reff: ')
     for (var i = 0; i < jumlah; i++) {
     try {
+        const getName = await functionName()
+        const number = Math.floor(Math.random() * 10000) + 100;
+        const username = `${getName.name}${number}`
         var jenis = [   
             "male",
             "female",
@@ -71,9 +86,9 @@ const functionRegist = (realCookie, kelamin) => new Promise((resolve, reject) =>
         const ad = cook[3].split(';')[0]
         const us = cook[5].split(';')[0]
         const realCookie = `${session}; ${ad}; mode=day; access=1; src=1; _ga=GA1.2.79560195.1575427248; _gid=GA1.2.1128287706.1575427248; _gat_gtag_UA_148732261_1=1; ${us}`
-        const regist = await functionRegist(realCookie, kelamin)
+        const regist = await functionRegist(username, realCookie, kelamin)
         if (regist.status == 200) {
-            console.log('[+] Register berhasil!')
+            console.log('[+] Register berhasil!\n')
         } else {
             console.log(regist)
         }
